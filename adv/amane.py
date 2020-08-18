52d322af8ee708ed7224d5ae26752cab50c8fd4a
souran <54129583+pfleg@users.noreply.github.com>
2020-08-18 02:30:20 -0700

    Update amane.py

--- adv/amane.py
+++ adv/amane.py
@@ -4,19 +4,28 @@ def module():
     return Amane
 
 class Amane(Adv):
-    a3 = ('bk',0.2)
-    a1 = ('prep',0.75)
+    a3 = ('bk',0.35)
+    a1 = [('prep',1.00), ('scharge_all', 0.05)]
     
     conf = {}
     conf['acl'] = """
         `dragon
         `s2
         `s1
-        `s3, x=5
-        `s4, x=5
+        `s3
+        `s4
         """
-    coab = ['Blade','Halloween_Elisanne','Peony']
-    share = ['Ranzal']
+    coab = ['Blade','Sharena','Peony']
+    conf['afflict_res.paralysis'] = 0
+    share = ['Summer_Patia', 'Ranzal']
+    
+     def init(self):
+        self.s1_stance = 1
+
+    def s1_proc(self, e):
+        self.afflics.paralysis(e.name,120, 0.97)
+
+    def s2_proc(self, e):
 
 
 if __name__ == '__main__':
